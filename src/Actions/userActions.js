431f7ac2b4e9673236a7cfbf503ed6095bdd489f
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-10 19:24:56 -0800

    add loginUser to src/Actions/userActions.js

--- src/Actions/userActions.js
+++ src/Actions/userActions.js
@@ -16,7 +16,6 @@ export const registerUser = (userFormData) => {
         fetch('http://localhost:3000/users', 
         {
           method: 'POST', 
-          credentials: 'include', 
           headers: {
               'Content-Type': 'application/json'
           },
@@ -39,6 +38,40 @@ export const registerUser = (userFormData) => {
                 localStorage.setItem("loggedIn", JSON.stringify(resp.logged_in))
                 dispatch(addUser(resp))
                 dispatch(updateCart(resp.cart))
+            }
+        })
+    }
+}
+
+export const loginUser = (userCredentials) => {
+
+    const { email, password } = userCredentials;
+    
+    return (dispatch) => {
+
+        fetch('http://localhost:3000/login', 
+        {
+          method: 'POST', 
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              user: {
+                  email: email,
+                  password: password
+              }
+          })
+        }) 
+        .then(resp => resp.json())
+        .then(resp => {
+            if(resp.logged_in === true) {
+                console.log(resp)
+                localStorage.setItem("user", JSON.stringify(resp.user))
+                localStorage.setItem("cart", JSON.stringify(resp.cart))
+                localStorage.setItem("loggedIn", JSON.stringify(resp.logged_in))
+
+                dispatch(addUser(resp));
+                dispatch(updateCart(resp));
             }
         })
     }

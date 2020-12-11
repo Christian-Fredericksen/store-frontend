6c81b4a4557249793481d6562ebd80f86df20598
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-11 13:15:02 -0800

    add and def cartActions to src/Actions

---
+++ src/Actions/cartActions.js
@@ -0,0 +1,68 @@
+export const removeFromCart = (item, cartId) => {
+    return (dispatch) => {
+        fetch(`http://localhost:3000/carts/${cartId}`, {
+            method: 'DELETE', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                item: {
+                    itemId: item.id
+                }
+            })
+       })
+       .then(resp => resp.json())
+       .then(resp => {
+        localStorage.setItem("cart", JSON.stringify(resp.cart))
+        dispatch(updateCart(resp))
+        })
+    }
+}
+
+export const placeOrder = () => {
+    
+    return (dispatch) => {
+       dispatch({type: 'PLACING_ORDER'})
+       fetch('http://localhost:3000/orders', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            }
+       })
+       .then(resp => resp.json())
+       .then(res => {
+            localStorage.setItem("cart", JSON.stringify(res.cart))
+            dispatch(updateCart(res))
+        })
+    }
+}
+
+export const updateCart = (cart) => {
+    return {
+        type: 'UPDATE_CART',
+        payload: cart
+    }
+}
+
+export const addItemToCart = (item, cartId) => {
+    return (dispatch) => {
+        dispatch({type:'ADDING_TO_CART'})
+        fetch('http://localhost:3000/carts', 
+        {
+          method: 'POST', 
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+           item: item,
+           cart_id: cartId
+          }),
+        })
+        .then(resp => resp.json())
+        .then(res => {
+            console.log(res)
+            localStorage.setItem("cart", JSON.stringify(res.cart))
+            dispatch(updateCart(res))
+        })
+    }
+}

7d72b45afc5295dab643142298f6a82eb8eba6da
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-11 13:21:31 -0800

    add home.js to src/Containers

---
+++ src/Containers/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+class Home extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Homepage</h1>
+                <img src="https://source.unsplash.com/Oh8Lp49y2Do/1000x700"></img>
+            </div>
+        )
+    }
+}
+
+
+export default Home;

d95bb52522b7a842220870fb126e926a559b1982
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-11 12:53:48 -0800

    add itemsList to Containers

---
+++ src/Containers/itemsList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Item from '../components/Item';
+import {addItemToCart} from '../actions/cartActions'
+// import {deleteCartItem} from '../actions/cartActions
+
+const ItemsList = (props) => {
+
+    const handleAddToCartClick = (item) => {
+        // console.log(`Adding ${item.name} to cart`)
+       props.addItemToCart(item, props.cartId) 
+    }
+
+    const buttonText = "Add to cart"
+
+    const items = props.items.map(item => <Item item={item} handleButtonClick={() => handleAddToCartClick(item)} buttonText={buttonText}/>)
+    
+    return(
+        <div>
+            {items}
+        </div>
+    )
+}
+
+const mapDispatchToProps = dispatch => ({
+    addItemToCart: (item, cartId) => dispatch(addItemToCart(item, cartId))
+});
+
+const mapStateToProps = state => ({
+     items: state.items.itemList,
+     cartId: state.cart.cartId
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemsList);

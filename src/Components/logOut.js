0b32ff71201d944b00247660d77f885cddadaee8
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-10 19:32:18 -0800

    add logOut.js to src/Components

---
+++ src/Components/logOut.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logout = (props) => {
+    return (
+        <button className="logout-btn" onClick={props.handleLogoutClick}>Logout</button>
+    )
+}
+
+export default Logout;

9a47c73025cb4e6757909b544dbd4b06a26c8708
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-11 13:08:59 -0800

    add cart.js to Components

---
+++ src/Components/cart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Item from './Item'
+
+const Cart = (props) => {
+    // expecting to receive in props:
+    // a function to handle submitting order.
+    // a function to handle removing an item from cart. (will pass this on to Item) 
+    const buttonText = "Remove Item"
+    const cartItems = props.cartItems.map((cartItem) => <Item item={cartItem} handleButtonClick={() => props.handleButtonClick(cartItem)} buttonText={buttonText} />)
+    // you will pass Item the callback fn to handle deleting an item. handleButtonClick = props.handleDeleteItem(cartItem).
+    // over here you should also pass in button text. 
+    
+    return(
+        <div>
+            <h4>Shopping Cart</h4>
+            <h3>Cart total: ${props.total}</h3>
+
+            {cartItems}
+
+            <button onClick={props.submitOrder}>Place Order</button>
+        </div>
+    )
+}
+
+export default Cart;

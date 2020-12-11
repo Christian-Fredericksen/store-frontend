bff2d323ccde1ccbcef44cb014744d2e438902bc
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-11 13:18:31 -0800

    add and def idex.js to src/Reducers

---
+++ src/Reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import usersReducer from './usersReducer';
+import itemsReducer from './itemsReducer';
+import cartReducer from './cartReducer';
+
+export default combineReducers({
+    user: usersReducer,
+    items: itemsReducer,
+    cart: cartReducer
+})

8ba8b50c8874265e4302f149f59dfcdc38dda095
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-11 12:59:13 -0800

    add itemActions to Actions

---
+++ src/Actions/itemActions.js
@@ -0,0 +1,18 @@
+function addItems(items) {
+    return {
+        type:'ADD_ITEMS',
+        payload: items
+    }
+}
+
+export const fetchItems = () => {
+    return (dispatch) => {
+        dispatch({type: 'FETCHING_ITEMS'})
+        
+        fetch('http://localhost:3000/items', {credentials: 'include'}) 
+        .then(resp => resp.json())
+        .then(resp => {
+            dispatch(addItems(resp))
+        })
+    }
+}
